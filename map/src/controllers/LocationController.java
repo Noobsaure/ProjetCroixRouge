4080b2caacb2c0b65ffa3446362456ac64903baf
Florian Leloup <florian.leloup.etu@gmail.com>
2013-04-26 17:02:38 +0200

    no message

--- map/src/controllers/LocationController.java
+++ map/src/controllers/LocationController.java
@@ -151,7 +151,7 @@ public class LocationController implements Subject {
 		java.sql.Timestamp datetime = new java.sql.Timestamp(date.getTime());
 
 		try {
-			_dbm.executeQueryInsert(new SQLQueryInsert("Message" ,"(NULL,'-1','-1','-1','-1',"+_operation.getIdOperateur()+", NULL, "+_operation.getId()+",NULL, NULL,'"+datetime+"','"+message+"',0)"));
+			_dbm.executeQueryInsert(new SQLQueryInsert("Message" ,"(NULL,NULL,NULL,'-1','-1',"+_operation.getIdOperateur()+", NULL, "+_operation.getId()+",NULL, NULL,'"+datetime+"','"+message+"',0)"));
 		} catch (MalformedQueryException e) {
 			new ErrorMessage(_operation.getGlobalPanel().getMapPanel(),"Erreur generation message" ,"Une erreur est survenue lors de la génération du message pour la main courante \n"+
 					"Message : "+message);
