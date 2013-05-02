b50f12c3d8042cdceb87c083b7da432bc4d0fee3
Noobsaure <Noobsaure@gmail.com>
2013-05-02 18:42:42 +0200

    ousplol

--- map/src/views/listeners/RemoveEquipierListener.java
+++ map/src/views/listeners/RemoveEquipierListener.java
@@ -3,25 +3,14 @@ package views.listeners;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JPanel;
-
-import views.ConfigurationEntityPanel;
-import views.MapPanel;
-
 import controllers.EntityController;
-import controllers.OperationController;
 import controllers.TeamMemberController;
 
 
 public class RemoveEquipierListener implements ActionListener{
 	
-	private MapPanel _mapPanel;
-	private JPanel _parent;
-	private JPanel _nomEquipierPanel;
-	private OperationController _operationController;
 	private TeamMemberController _equipier;
 	private EntityController _entityController;
-	private ConfigurationEntityPanel _configEntityPanel;
 	
 	public RemoveEquipierListener(EntityController entityController, TeamMemberController equipier) {
 		_entityController = entityController;
