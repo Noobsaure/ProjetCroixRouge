8583b7b6125c9f2420ffe744501a30fc03f4069c
Arnaud <arnaud.lelouet@yahoo.fr>
2013-05-06 15:27:16 +0200

    ConfigurationEntityPanel.java

--- map/src/views/ConfigurationEntityPanel.java
+++ map/src/views/ConfigurationEntityPanel.java
@@ -37,6 +37,8 @@ import controllers.LocationController;
 import controllers.MapController;
 import controllers.OperationController;
 import controllers.TeamMemberController;
+import java.awt.FlowLayout;
+import java.awt.Rectangle;
 
 public class ConfigurationEntityPanel extends CustomPanelImpl
 {
@@ -81,6 +83,7 @@ public class ConfigurationEntityPanel extends CustomPanelImpl
 		setOpaque(false);
 
 		JPanel internalPanel = new PopUpPanel();
+		internalPanel.setLocation(0, 11);
 		internalPanel.setLayout(new BoxLayout(internalPanel,BoxLayout.PAGE_AXIS));
 		internalPanel.setBorder(new EmptyBorder(5,5,5,5));
 		add(internalPanel);
@@ -89,58 +92,37 @@ public class ConfigurationEntityPanel extends CustomPanelImpl
 		title.setAlignmentX(CENTER_ALIGNMENT);
 		title.setBorder(new EmptyBorder(0,0,10,0));
 		internalPanel.add(title);
-
-		/**************************************************************\
-		 * 							Nom
-		\**************************************************************/
-		JPanel namePanel = new JPanel();
-		namePanel.setLayout(new BoxLayout(namePanel,BoxLayout.LINE_AXIS));
-		namePanel.setBorder(new EmptyBorder(5,0,5,0));
 		Dimension labelSize = new Dimension(100,25);
 		Dimension fieldSize = new Dimension(240,25);
 		Dimension buttonSize = new Dimension(40,25);
-
-		JLabel nameLabel = new JLabel("Nom:");
-		nameLabel.setBorder(new EmptyBorder(0,5,0,5));
-		nameLabel.setMinimumSize(labelSize);
-		nameLabel.setMaximumSize(labelSize);
-		nameLabel.setPreferredSize(labelSize);
-		nameLabel.setSize(labelSize);
-		namePanel.add(nameLabel);
-
-		_nameTextField = new JTextField(_entityController.getName());
-		_nameTextField.setBorder(new EmptyBorder(0,5,0,5));
-		_nameTextField.setMinimumSize(fieldSize);
-		_nameTextField.setMaximumSize(fieldSize);
-		_nameTextField.setPreferredSize(fieldSize);
-		namePanel.add(_nameTextField);
-
-		CustomButton editNameEntityButton = new CustomButton("Ok");
-		editNameEntityButton.setBorder(new EmptyBorder(0,5,0,5));
-		editNameEntityButton.setMinimumSize(buttonSize);
-		editNameEntityButton.setMaximumSize(buttonSize);
-		editNameEntityButton.setPreferredSize(buttonSize);
-		editNameEntityButton.addActionListener(new EditEntityNameButtonListener(_mapPanel, _entityController, this));
-		namePanel.add(editNameEntityButton);
-
-		internalPanel.add(namePanel);
-
-		/**************************************************************/
-
-
-		/**************************************************************\
-		 * 						Localisation
-		\**************************************************************/
-		JPanel locPanel = new JPanel();
-		locPanel.setLayout(new BoxLayout(locPanel,BoxLayout.LINE_AXIS));
-		locPanel.setBorder(new EmptyBorder(0,0,5,0));
-
-		JLabel locLabel = new JLabel("Localisation:");
-		locLabel.setBorder(new EmptyBorder(0,5,0,5));
-		locLabel.setMinimumSize(labelSize);
-		locLabel.setMaximumSize(labelSize);
-		locLabel.setPreferredSize(labelSize);
-		locPanel.add(locLabel);
+		
+		JPanel panel_Nom = new JPanel();
+		panel_Nom.setPreferredSize(new Dimension(0, 0));
+		internalPanel.add(panel_Nom);
+		panel_Nom.setLayout(new BorderLayout(0, 0));
+		
+				JLabel nameLabel = new JLabel("Nom:");
+				panel_Nom.add(nameLabel, BorderLayout.WEST);
+				nameLabel.setBorder(new EmptyBorder(0,5,0,5));
+				nameLabel.setMinimumSize(labelSize);
+				nameLabel.setMaximumSize(labelSize);
+				nameLabel.setPreferredSize(labelSize);
+				nameLabel.setSize(labelSize);
+				
+						_nameTextField = new JTextField(_entityController.getName());
+						panel_Nom.add(_nameTextField, BorderLayout.CENTER);
+						_nameTextField.setBorder(new EmptyBorder(0,5,0,5));
+						_nameTextField.setMinimumSize(fieldSize);
+						_nameTextField.setMaximumSize(fieldSize);
+						_nameTextField.setPreferredSize(fieldSize);
+						
+								CustomButton editNameEntityButton = new CustomButton("Ok");
+								panel_Nom.add(editNameEntityButton, BorderLayout.EAST);
+								editNameEntityButton.setBorder(new EmptyBorder(0,5,0,5));
+								editNameEntityButton.setMinimumSize(buttonSize);
+								editNameEntityButton.setMaximumSize(buttonSize);
+								editNameEntityButton.setPreferredSize(buttonSize);
+								editNameEntityButton.addActionListener(new EditEntityNameButtonListener(_mapPanel, _entityController, this));
 
 		Vector<String> comboBoxItems = new Vector<String>();
 
@@ -171,62 +153,74 @@ public class ConfigurationEntityPanel extends CustomPanelImpl
 		final DefaultComboBoxModel model = new DefaultComboBoxModel(comboBoxItems);
 
 		model.setSelectedItem(selectedLocation);
-
-		_typeComboBox = new JComboBox<String>(model);
-		_typeComboBox.setMinimumSize(fieldSize);
-		_typeComboBox.setMaximumSize(fieldSize);
-		_typeComboBox.setPreferredSize(fieldSize);
-		locPanel.add(_typeComboBox);
-
-		CustomButton editLocationEntityButton = new CustomButton("Ok");
-		editLocationEntityButton.addActionListener(new EditEntityLocationButtonListener(_entityController, this));
+		
+		JPanel panel_Localisation = new JPanel();
+		panel_Localisation.setSize(new Dimension(400, 10));
+		panel_Localisation.setMinimumSize(new Dimension(400, 10));
+		panel_Localisation.setPreferredSize(new Dimension(0, 0));
+		internalPanel.add(panel_Localisation);
+		panel_Localisation.setLayout(new BorderLayout(0, 0));
+		
+				JLabel locLabel = new JLabel("Localisation:");
+				panel_Localisation.add(locLabel, BorderLayout.WEST);
+				locLabel.setBorder(new EmptyBorder(0,5,0,5));
+				locLabel.setMinimumSize(labelSize);
+				locLabel.setMaximumSize(labelSize);
+				locLabel.setPreferredSize(new Dimension(100, 15));
+				
+						_typeComboBox = new JComboBox<String>(model);
+						_typeComboBox.setSize(new Dimension(240, 15));
+						panel_Localisation.add(_typeComboBox, BorderLayout.CENTER);
+						_typeComboBox.setMinimumSize(new Dimension(240, 15));
+						_typeComboBox.setMaximumSize(fieldSize);
+						_typeComboBox.setPreferredSize(new Dimension(240, 15));
+						
+								CustomButton editLocationEntityButton = new CustomButton("Ok");
+								panel_Localisation.add(editLocationEntityButton, BorderLayout.EAST);
+								editLocationEntityButton.addActionListener(new EditEntityLocationButtonListener(_entityController, this));
 		editLocationEntityButton.setBorder(new EmptyBorder(0,5,0,5));
 		editLocationEntityButton.setMinimumSize(buttonSize);
 		editLocationEntityButton.setMaximumSize(buttonSize);
-		editLocationEntityButton.setPreferredSize(buttonSize);
-		locPanel.add(editLocationEntityButton);
-
-		internalPanel.add(locPanel);
-		/**************************************************************/
-
-
-
-		/**************************************************************\
-		 * 						Liste des equipiers
-		\**************************************************************/
-
+		editLocationEntityButton.setPreferredSize(new Dimension(40, 15));
+		
+		JPanel panel_5 = new JPanel();
+		internalPanel.add(panel_5);
+		
 		JSeparator separator = new JSeparator();
-		internalPanel.add(separator);
-		JLabel listeEquipierLabel = new JLabel("Liste Equipiers:", JLabel.LEFT);
-		listeEquipierLabel.setAlignmentX(LEFT_ALIGNMENT);
-		internalPanel.add(listeEquipierLabel);
-
-		_listeEquipierPanel = new JPanel();
-
-		JScrollPane scrollPane = new JScrollPane(_listeEquipierPanel);
-		scrollPane.setMaximumSize(new Dimension(250, 200));
-		scrollPane.setMinimumSize(new Dimension(250, 200));
-		scrollPane.setPreferredSize(new Dimension(250, 200));
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-
-		internalPanel.add(scrollPane);
-
-		_listeEquipierPanel.setLayout(new BoxLayout(_listeEquipierPanel, BoxLayout.Y_AXIS));
+		separator.setPreferredSize(new Dimension(400, 2));
+		panel_5.add(separator);
+		
+		JPanel panel_Liste_Equipier = new JPanel();
+		internalPanel.add(panel_Liste_Equipier);
+		panel_Liste_Equipier.setLayout(new BorderLayout(0, 0));
+		
+				CustomButton AjoutEquipierButton = new CustomButton("Ajouter un equipier");
+				panel_Liste_Equipier.add(AjoutEquipierButton, BorderLayout.SOUTH);
+				AjoutEquipierButton.addActionListener(new AddEquipierButtonListener(_mapPanel,_operationController, _entityController));
+				AjoutEquipierButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+				
+						_listeEquipierPanel = new JPanel();
+						
+								JScrollPane scrollPane = new JScrollPane(_listeEquipierPanel);
+								panel_Liste_Equipier.add(scrollPane, BorderLayout.CENTER);
+								scrollPane.setMaximumSize(new Dimension(250, 200));
+								scrollPane.setMinimumSize(new Dimension(250, 200));
+								scrollPane.setPreferredSize(new Dimension(250, 200));
+								scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+								scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+								
+										_listeEquipierPanel.setLayout(new BoxLayout(_listeEquipierPanel, BoxLayout.Y_AXIS));
+										
+										JPanel panel_1 = new JPanel();
+										panel_Liste_Equipier.add(panel_1, BorderLayout.WEST);
+										panel_1.setLayout(new BorderLayout(0, 0));
+										JLabel listeEquipierLabel = new JLabel("Liste Equipiers:", JLabel.LEFT);
+										panel_1.add(listeEquipierLabel, BorderLayout.NORTH);
+										listeEquipierLabel.setAlignmentY(Component.TOP_ALIGNMENT);
+										listeEquipierLabel.setPreferredSize(new Dimension(100, 14));
+										listeEquipierLabel.setAlignmentX(LEFT_ALIGNMENT);
 
 		setEquipiersList();
-
-		CustomButton AjoutEquipierButton = new CustomButton("Ajouter un equipier");
-		AjoutEquipierButton.addActionListener(new AddEquipierButtonListener(_mapPanel,_operationController, _entityController));
-		AjoutEquipierButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-		internalPanel.add(AjoutEquipierButton);
-
-		/**************************************************************\
-		 * 					Buttons radio Dispo/Indispo
-		\**************************************************************/
-
-		separator = new JSeparator();
-		internalPanel.add(separator);
 
 		Dimension dispoPanelDimension = new Dimension(125,50);
 		Dimension scrollPanelDimension = new Dimension(250,100);
@@ -234,12 +228,6 @@ public class ConfigurationEntityPanel extends CustomPanelImpl
 
 		JPanel dispoPanel = new JPanel();
 		dispoPanel.setLayout(new BoxLayout(dispoPanel, BoxLayout.LINE_AXIS));
-		
-		JLabel statutLabel = new JLabel("Statut :");
-		statutLabel.setMaximumSize(dispoPanelDimension);
-		statutLabel.setMinimumSize(dispoPanelDimension);
-		statutLabel.setPreferredSize(dispoPanelDimension);
-		dispoPanel.add(statutLabel);
 
 		if (_entityController.getStatut()) {
 			_disponibleStatutRadioButton.setSelected(true);
@@ -248,73 +236,120 @@ public class ConfigurationEntityPanel extends CustomPanelImpl
 			_disponibleStatutRadioButton.setSelected(false);
 			_indisponibleStatutRadioButton.setSelected(true);
 		}
+		
+		JPanel panel_2 = new JPanel();
+		internalPanel.add(panel_2);
+		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setPreferredSize(new Dimension(400, 2));
+		panel_2.add(separator_1);
+		
+		JPanel panel_Statut = new JPanel();
+		panel_Statut.setPreferredSize(new Dimension(10, 20));
+		internalPanel.add(panel_Statut);
+		panel_Statut.setLayout(new BorderLayout(0, 0));
 
 		JPanel panel = new JPanel();
+		panel_Statut.add(panel, BorderLayout.CENTER);
 		panel.setMaximumSize(dispoPanelDimension);
 		panel.setMinimumSize(dispoPanelDimension);
-		panel.setPreferredSize(dispoPanelDimension);
+		panel.setPreferredSize(new Dimension(125, 20));
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-
-		dispoPanel.add(panel);
 		panel.add(_disponibleStatutRadioButton);
 
 		JradioBoutonGroup.add(_disponibleStatutRadioButton);
 		panel.add(_indisponibleStatutRadioButton);
 
 		JradioBoutonGroup.add(_indisponibleStatutRadioButton);
+		
+		JPanel panel_3 = new JPanel();
+		panel_Statut.add(panel_3, BorderLayout.WEST);
+		panel_3.setLayout(new BorderLayout(0, 0));
+		
+		JLabel statutLabel = new JLabel("Statut :");
+		panel_3.add(statutLabel, BorderLayout.NORTH);
+		statutLabel.setAlignmentY(Component.TOP_ALIGNMENT);
+		statutLabel.setMaximumSize(dispoPanelDimension);
+		statutLabel.setMinimumSize(dispoPanelDimension);
+		statutLabel.setPreferredSize(new Dimension(100, 20));
 
 		internalPanel.add(dispoPanel);
-
-		JLabel infoLabel = new JLabel("Informations :");
-		infoLabel.setMaximumSize(infoLabelDimension);
-		infoLabel.setMinimumSize(infoLabelDimension);
-		infoLabel.setPreferredSize(infoLabelDimension);
-		internalPanel.add(infoLabel);
+		
+		JPanel panel_Informations = new JPanel();
+		internalPanel.add(panel_Informations);
+		panel_Informations.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_4 = new JPanel();
+		panel_Informations.add(panel_4, BorderLayout.WEST);
+		panel_4.setLayout(new BorderLayout(0, 0));
+		
+				JLabel infoLabel = new JLabel("Informations :");
+				panel_4.add(infoLabel, BorderLayout.NORTH);
+				infoLabel.setAlignmentY(Component.TOP_ALIGNMENT);
+				infoLabel.setMaximumSize(infoLabelDimension);
+				infoLabel.setMinimumSize(infoLabelDimension);
+				infoLabel.setPreferredSize(new Dimension(100, 25));
 
 		JScrollPane textAreaScrollPane = new JScrollPane();
+		panel_Informations.add(textAreaScrollPane, BorderLayout.CENTER);
 		textAreaScrollPane.setMaximumSize(scrollPanelDimension);
 		textAreaScrollPane.setMinimumSize(scrollPanelDimension);
-		textAreaScrollPane.setPreferredSize(scrollPanelDimension);
-		internalPanel.add(textAreaScrollPane);
+		textAreaScrollPane.setPreferredSize(new Dimension(250, 50));
 
 		_informationsTextArea = new JTextArea(_entityController.getInformationsStatut());
+		_informationsTextArea.setPreferredSize(new Dimension(200, 26));
 		textAreaScrollPane.setViewportView(_informationsTextArea);
 
 		CustomButton modifStatusButton = new CustomButton("Valider");
+		panel_Informations.add(modifStatusButton, BorderLayout.SOUTH);
 		modifStatusButton.addActionListener(new EditStatusEntityButtonListener(_entityController, this));
-		internalPanel.add(modifStatusButton);
-
-		separator = new JSeparator();
-		internalPanel.add(separator);
 
 		/**************************************************************\
 		 * 						Couleur
 		\**************************************************************/
 
-		JLabel lblCouleur = new JLabel("Couleur :");
-		internalPanel.add(lblCouleur);
-
-		_colorChooserPanel = new JPanel();
-		_colorChooserPanel.addMouseListener(new ColorChooserListener(_colorChooserPanel));
-
 		String stringColor = _entityController.getColor();
 
 		_colorEntity = stringToColor(stringColor);
+		
+		JPanel panel_7 = new JPanel();
+		internalPanel.add(panel_7);
+		
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setPreferredSize(new Dimension(400, 2));
+		panel_7.add(separator_2);
+		
+		JPanel panel_Couleur = new JPanel();
+		panel_Couleur.setPreferredSize(new Dimension(0, 0));
+		internalPanel.add(panel_Couleur);
+		panel_Couleur.setLayout(new BorderLayout(0, 0));
+						
+						JPanel panel_6 = new JPanel();
+						panel_Couleur.add(panel_6, BorderLayout.WEST);
+						
+								JLabel lblCouleur = new JLabel("Couleur :");
+								panel_6.add(lblCouleur);
+				
+						_colorChooserPanel = new JPanel();
+						_colorChooserPanel.setPreferredSize(new Dimension(10, 0));
+						panel_Couleur.add(_colorChooserPanel, BorderLayout.CENTER);
+						_colorChooserPanel.addMouseListener(new ColorChooserListener(_colorChooserPanel));
 
 		_colorChooserPanel.setBackground(_colorEntity);
 
-		internalPanel.add(_colorChooserPanel);
-
 		CustomButton ModifColorButton = new CustomButton("Valider");
+		ModifColorButton.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+		panel_Couleur.add(ModifColorButton, BorderLayout.SOUTH);
 		ModifColorButton.addActionListener(new EditEntityColorListener(_mapPanel,this, _entityController));
-		internalPanel.add(ModifColorButton);
-
-		separator = new JSeparator();
-		internalPanel.add(separator);
-
-		/**************************************************************\
-		 * 						Button retour
-		\**************************************************************/
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setSize(new Dimension(0, 30));
+		internalPanel.add(panel_8);
+		
+		JSeparator separator_3 = new JSeparator();
+		separator_3.setPreferredSize(new Dimension(400, 2));
+		panel_8.add(separator_3);
 
 		JPanel buttonPanel = new JPanel();
 		internalPanel.add(buttonPanel);
@@ -325,7 +360,7 @@ public class ConfigurationEntityPanel extends CustomPanelImpl
 
 		/**************************************************************/
 
-		internalPanel.setSize(DIMENSION_PANEL);
+		internalPanel.setSize(new Dimension(400, 680));
 		setPreferredSize(internalPanel.getSize());
 	}
 
